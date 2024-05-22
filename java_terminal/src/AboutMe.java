f8ed182db52a358751eda63eb28f3ddd1695300c
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2024-05-22 11:40:06 -0300

    Aula sobre o uso do Scanner

--- java_terminal/src/AboutMe.java
+++ java_terminal/src/AboutMe.java
@@ -1,9 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args [2]);
-        double altura =  Double.valueOf(args [3]);
+
+        //criando o objeto scanner
+        Scanner digite = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite sua altura: ");
+        double altura = digite.nextDouble();
+        
+        System.out.print("Digite seu nome: : ");
+        String nome = digite.next();
+
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = digite.next();
+
+        System.out.print("Digite sua idade: ");
+        int idade = digite.nextInt();
+
+
 
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
