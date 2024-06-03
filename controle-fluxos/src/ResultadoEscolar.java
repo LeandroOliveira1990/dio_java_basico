af6c039bb57b46302df7d134c71deef70d866303
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2024-06-03 16:09:38 -0300

    Condicional encadeada ResultadoEscolar

--- controle-fluxos/src/ResultadoEscolar.java
+++ controle-fluxos/src/ResultadoEscolar.java
@@ -1,12 +1,19 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 6;
 
         if (nota >= 7) {
 
             System.out.println("Aprovado");
             
-        } else {
+        } 
+        else if(nota >= 5 && nota < 7 ){
+            System.out.println("Prova de Recuperação");
+
+        }
+        
+
+        else {
             System.out.println("Reprovado");
         }
     }
