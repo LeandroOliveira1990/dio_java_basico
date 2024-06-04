16a168ce2887552b58d27bbc0565f5fc08177c45
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2024-06-04 15:06:11 -0300

    Aula sobre usar For com Arrays, ExemploForArray , ExemploForEach

---
+++ controle-fluxos/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no indice [" + i + "] "  + "é = " + alunos[i]  );
+        }
+    }
+}
